467e1ea45a85e51f279af11a52bfe426ba172582
fbansept <bansept.franck@gmail.com>
2023-06-08 13:14:20 +0200

    ajout theme

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import AppStyles from "./AppStyles";
 
 export default function App() {
+  const styles = AppStyles();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ theme.js
@@ -0,0 +1,25 @@
+import { useColorScheme } from "react-native";
+
+const colors = {
+  primary: "#467386",
+  accent: "#D5A26A",
+  warn: "#A7373F",
+  white: "#fff",
+  black: "#000",
+};
+
+const themeColors = {
+  light: {
+    backgroundColor: colors.white,
+    textColor: colors.black,
+  },
+  dark: {
+    backgroundColor: "#222",
+    textColor: colors.white,
+  },
+};
+
+export default () => {
+  const colors = themeColors[useColorScheme()];
+  return { ...colors, ...themeColors };
+};
