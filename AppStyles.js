4926b9409e58734b33316ec78ff5db5a6a2d0994
fbansept <bansept.franck@gmail.com>
2023-06-08 13:26:52 +0200

    fix

--- AppStyles.js
+++ AppStyles.js
@@ -1,24 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { colors } from "./theme";
+import { StyleSheet } from "react-native";
+import theme from "./theme";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function createStyles() {
+  const colors = theme();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.backgroundColor,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
+  return styles;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.backgroundColor,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: colors.textColor,
-  },
-});
