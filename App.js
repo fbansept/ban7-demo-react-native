a94bce8ba30b7dec65bf1e8d9acc122861efec51
fbansept <bansept.franck@gmail.com>
2023-06-09 19:04:30 +0200

    perpae part 3

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import AppStyles from "./AppStyles";
 
 export default function App() {
   const styles = AppStyles();
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      setIsLoggedIn(true);
+      console.log("Connexion réussie !");
+    } catch (error) {
+      console.error("Erreur lors de la connexion :", error);
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const AppNavigator = () => {
+    return <Text>Navigation Login</Text>;
+  };
+
+  const LoginNavigator = () => {
+    return <Text>Navigation Application</Text>;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      {isLoggedIn ? <AppNavigator /> : <LoginNavigator />}
+    </NavigationContainer>
   );
 }
