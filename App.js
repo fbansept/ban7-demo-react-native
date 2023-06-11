078f1bb37403cea1e05c849dbc62d07b1939dfe4
fbansept <bansept.franck@gmail.com>
2023-06-11 15:51:47 +0200

    ajout safeArea

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import AppStyles from "./AppStyles";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const styles = AppStyles();
+  const App = () => {
+    const styles = AppStyles();
+
+    return (
+      <View style={[styles.container, styles.safeArea, styles.centered]}>
+        <Text style={styles.text}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <App></App>
+    </SafeAreaProvider>
   );
 }
